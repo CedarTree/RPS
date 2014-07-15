2064d196cd0a327f5a5403dc95acc33a197f4028
Sinclair <sbk64@cornell.edu>
2014-07-15 17:05:04 -0400

    constant

--- rps.rb
+++ rps.rb
@@ -1,43 +1,43 @@
 class RPS 
+  MOVES = ['r', 's', 'p']
+
+  def play
+    loop do
+      input 
+       if @player == "q"
+        break  
+       end
+      output
+      show_result
+    end
+  end
 
   def display_intro 
-   puts  "*****************************"
-   puts  "Rock, Paper, Scissors:"
-   puts  "Your move? (R/P/S, q to quit)" 
+    puts  "*****************************"
+    puts  "Rock, Paper, Scissors:"
+    puts  "Your move? (R/P/S, q to quit)" 
   end
 
   def input
     display_intro
-
-    @computer = "rsp"[rand(3)].chr
+    @computer = MOVES.sample
     @player = $stdin.gets.chomp.downcase
   end
 
   def output
-      case [@player, @computer]
-           when ['p','r'], ['s','p'], ['r','s']
-             puts "Player beats AI"
-           when ['r','r'], ['s','s'], ['p','p']
-             puts "Tie"
-           else
-             puts "AI beats Player"
-      end
+    case [@player, @computer]
+      when ['p','r'], ['s','p'], ['r','s']
+       puts "Player beats AI"
+      when ['r','r'], ['s','s'], ['p','p']
+       puts "Tie"
+      else
+       puts "AI beats Player"
+    end
   end
 
   def show_result
-      puts "Player played: #{@player.upcase} "
-      puts "AI played: #{@computer.upcase}" 
-  end
-
-  def play
-    loop do
-     input 
-     if @player == "q"
-      break  
-     end
-     output
-     show_result
-    end
+    puts "Player played: #{@player.upcase} "
+    puts "AI played: #{@computer.upcase}" 
   end
 end 
 
